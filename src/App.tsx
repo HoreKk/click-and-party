a16ee40ee1e6587093d5034c2f6dc0d40f29fe9f
Antoine Lelong <azenoxe@gmail.com>
2025-05-24 22:25:52 +0200

    fix: add chakra ui and resolve alias path for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import "./App.css";
+import { Button } from "@chakra-ui/react";
 
 function App() {
-  return <></>;
+  return <Button>Click Me</Button>;
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Provider } from "@/components/ui/provider";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -5,6 +6,8 @@ import App from "./App.tsx";
 // biome-ignore lint/style/noNonNullAssertion: <explanation>
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </StrictMode>,
 );
