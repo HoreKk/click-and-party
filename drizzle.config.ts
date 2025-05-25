3036b80a05fc97e92b7b2750f4dbc9720072d34e
Antoine Lelong <azenoxe@gmail.com>
2025-05-25 11:00:03 +0200

    fix: dynamic drizzle-config and studio for local and remote env

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,18 +1,43 @@
+import fs from "node:fs";
+import path from "node:path";
 import type { Config } from "drizzle-kit";
+
+function getLocalD1DB() {
+  try {
+    const basePath = path.resolve(".wrangler");
+    const dbFile = fs
+      .readdirSync(basePath, { encoding: "utf-8", recursive: true })
+      .find((f) => f.endsWith(".sqlite"));
+
+    if (!dbFile) {
+      throw new Error(`.sqlite file not found in ${basePath}`);
+    }
+
+    const url = path.resolve(basePath, dbFile);
+    return url;
+  } catch (err) {
+    console.log(`Error  ${err}`);
+  }
+}
 
 const config: Config = {
   schema: "./drizzle/schema.ts",
-  out: ".drizzle-out",
+  out: "./drizzle/migrations",
   dialect: "sqlite",
-  driver: "d1-http",
-  dbCredentials: {
-    // biome-ignore lint/style/noNonNullAssertion: <explanation>
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-    // biome-ignore lint/style/noNonNullAssertion: <explanation>
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-    // biome-ignore lint/style/noNonNullAssertion: <explanation>
-    token: process.env.CLOUDFLARE_D1_TOKEN!,
-  },
+  ...(process.env.NODE_ENV === "production"
+    ? {
+        driver: "d1-http",
+        dbCredentials: {
+          accountId: process.env.CLOUDFLARE_ACCOUNT_ID,
+          databaseId: process.env.CLOUDFLARE_DATABASE_ID,
+          token: process.env.CLOUDFLARE_D1_TOKEN,
+        },
+      }
+    : {
+        dbCredentials: {
+          url: getLocalD1DB(),
+        },
+      }),
 };
 
 export default config satisfies Config;
